f09526013bdd557a42e9b85d3e001c67b8e9a8d5
Neal van Veen <Nvveen@users.noreply.github.com>
2025-12-04 16:15:48 +0000

    part 1

--- src/days/day01/index.ts
+++ src/days/day01/index.ts
@@ -1,14 +1,28 @@
 import { FileSystem } from "@effect/platform";
 import { BunContext } from "@effect/platform-bun";
-import { Console, Effect } from "effect";
+import { Console, Effect, Schema } from "effect";
 
-const program = Effect.gen(function* () {
+export const program = Effect.gen(function* () {
   const fs = yield* FileSystem.FileSystem;
   const input = yield* fs.readFileString("./input.txt");
-  console.log(input);
-  yield* Console.log("Hello World from Day 1!");
+  const parser = Schema.TemplateLiteralParser(Schema.Literal("L", "R"), Schema.Int);
+  const instructions = Array.from(
+    input.split("\n").map((line) => Schema.decodeUnknownSync(parser)(line.trim())),
+  );
+  // turn the dial, count the times we end up on 0. Remember, going negative or above 99 loops around
+  const [_, total] = instructions.reduce<[number, number]>(
+    ([pos, total], [turn, dist]) => {
+      pos = turn === "L" ? (pos - (dist % 100) + 100) % 100 : (pos + (dist % 100)) % 100;
+      return [pos, pos === 0 ? total + 1 : total];
+    },
+    [50, 0],
+  );
+  return total;
 });
 
-const main = program.pipe(Effect.provide(BunContext.layer));
+const main = program.pipe(
+  Effect.provide(BunContext.layer),
+  Effect.flatMap((result) => Console.log(`Total times at position 0: ${result}`)),
+);
 
 export default main;

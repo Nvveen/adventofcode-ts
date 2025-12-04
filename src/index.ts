a992ae15614c6ba7bfa718b878a5b9bb72ebf5a7
Neal van Veen <Nvveen@users.noreply.github.com>
2025-12-04 13:55:26 +0000

    add command

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-import { Command } from "@effect/cli";
+import { Args, Command } from "@effect/cli";
 import { BunContext, BunRuntime } from "@effect/platform-bun";
-import { Console, Effect } from "effect";
+import { Console, Effect, Schema } from "effect";
 
-const command = Command.make("aoc2025", {}, () => Console.log("Hello, world"));
+const dayNr = Args.integer({ name: "day number" }).pipe(
+  Args.withDescription("The day number to run"),
+  Args.withSchema(Schema.Number.pipe(Schema.greaterThan(0))),
+);
+const day = Command.make("day", { dayNr }, () => Console.log("Specify a day to run"));
+
+const command = Command.make("aoc2025", {}, () => Effect.fail("Please specify a subcommand")).pipe(
+  Command.withSubcommands([day]),
+);
 
 const cli = Command.run(command, {
   name: "aoc2025",
